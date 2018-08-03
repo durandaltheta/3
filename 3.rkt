11650f2fa5b082635a777b1531cdaa161bed4da9
Dennis <Blayne.Dennis@elektrobit.com>
2018-08-03 16:51:31 -0400

    cleanup

--- 3.rkt
+++ 3.rkt
@@ -32,7 +32,7 @@
 ;;; Test Functions (to run tests (define *run-3-tests #t) before loading)
 ;;;---------------------------------------------------------------------------- 
 ;; Return #t if the quoted form returns #t, else #f 
-(define (test-true form)
+(define (test-true? form)
   (if (eqv? form #t)
       (begin
         (printf "pass: ~a == #t" form)
@@ -43,7 +43,7 @@
 
 
 ;; Return #t if quoted forms return an equal value, else #f
-(define (test-equal form-a form-b)
+(define (test-equal? form-a form-b)
   (if (eqv? form-a form-b)
       (begin
         (printf "pass: ~a == ~a" form-a form-b)
@@ -120,10 +120,10 @@
     #f)
 
 
-  ;; Test the coroutine runs correctly
-  (test-equal (co-test1) 1)
-  (test-equal (co-test1) 2)
-  (test-true (not (co-test1)))
+  ;Test the coroutine runs correctly
+  (test-equal? (co-test1) 1)
+  (test-equal? (co-test1) 2)
+  (test-true? (not (co-test1)))
 
   (define-coroutine
     (co-test2 arg1 arg2)
@@ -131,15 +131,15 @@
     (yield arg2)
     #f)
 
-  ;; Test coroutine can be run with arguments
-  (test-equal (co-test2 "teststring" 'test)  "teststring")
-  (test-equal (co-test2)  'test)
-  (test-true (not co-test2))
+  ;Test coroutine can be run with arguments
+  (test-equal? (co-test2 "teststring" 'test)  "teststring")
+  (test-equal? (co-test2)  'test)
+  (test-true? (not co-test2))
 
-  ;; Test coroutine can be rerun with new arguments
-  (test-equal (co-test2 'test2 "teststring2")  'test2)
-  (test-equal (co-test2)  "teststring2")
-  (test-true (not co-test2)))
+  ;Test coroutine can be rerun with new arguments
+  (test-equal? (co-test2 'test2 "teststring2")  'test2)
+  (test-equal? (co-test2)  "teststring2")
+  (test-true? (not co-test2)))
 ;;**************************************
 
 
@@ -156,10 +156,12 @@
                                                    body
                                                    ...
                                                    #f))
-                              (raise 
-                                (make-exn:fail:user
-                                  '("Function is not pure: " 
-                                    (define (name . params) body ...)))))))
+                              (begin
+                                (raise 
+                                  (make-exn:fail:user
+                                    '("Function is not pure: " 
+                                      (define (name . params) body ...))))
+                                '()))))
 
 ;;TEST
 ;; Tests for (func) definitions
@@ -170,10 +172,10 @@
     (yield 2)
     #f)
 
-  ;; Test the coroutine runs correctly
-  (test-equal (co-test1) 1)
-  (test-equal (co-test1) 2)
-  (test-true (not (co-test1)))
+  ;Test the coroutine runs correctly
+  (test-equal? (co-test1) 1)
+  (test-equal? (co-test1) 2)
+  (test-true? (not (co-test1)))
 
   (func
     (co-test2 arg1 arg2)
@@ -181,17 +183,17 @@
     (yield arg2)
     #f)
 
-  ;; Test coroutine can be run with arguments
-  (test-equal (co-test2 "teststring" 'test)  "teststring")
-  (test-equal (co-test2)  'test)
-  (test-true (not co-test2))
+  ;Test coroutine can be run with arguments
+  (test-equal? (co-test2 "teststring" 'test)  "teststring")
+  (test-equal? (co-test2)  'test)
+  (test-true? (not co-test2))
 
-  ;; Test coroutine can be rerun with new arguments
-  (test-equal (co-test2 'test2 "teststring2")  'test2)
-  (test-equal (co-test2)  "teststring2")
-  (test-true (not co-test2))
+  ;Test coroutine can be rerun with new arguments
+  (test-equal? (co-test2 'test2 "teststring2")  'test2)
+  (test-equal? (co-test2)  "teststring2")
+  (test-true? (not co-test2))
 
-  ;; Should raise an exception due to the stateful (set!)
+  ;Should raise an exception due to the stateful (set!)
   (with-handlers ([exn:fail:user? (lambda (e) (test-pass e))])
                  (func 
                    (co-test3 arg1)
@@ -226,10 +228,10 @@
     (yield 2)
     #f)
 
-  ;; Test the coroutine runs correctly
-  (test-equal (co-test1) 1)
-  (test-equal (co-test1) 2)
-  (test-true (not (co-test1)))
+  ;Test the coroutine runs correctly
+  (test-equal? (co-test1) 1)
+  (test-equal? (co-test1) 2)
+  (test-true? (not (co-test1)))
 
   (func!
     (co-test2 arg1 arg2)
@@ -237,26 +239,26 @@
     (yield arg2)
     #f)
 
-  ;; Test coroutine can be run with arguments
-  (test-equal (co-test2 "teststring" 'test)  "teststring")
-  (test-equal (co-test2)  'test)
-  (test-true (not co-test2))
+  ;Test coroutine can be run with arguments
+  (test-equal? (co-test2 "teststring" 'test)  "teststring")
+  (test-equal? (co-test2)  'test)
+  (test-true? (not co-test2))
 
-  ;; Test coroutine can be rerun with new arguments
-  (test-equal (co-test2 'test2 "teststring2")  'test2)
-  (test-equal (co-test2)  "teststring2")
-  (test-true (not co-test2))
+  ;Test coroutine can be rerun with new arguments
+  (test-equal? (co-test2 'test2 "teststring2")  'test2)
+  (test-equal? (co-test2)  "teststring2")
+  (test-true? (not co-test2))
 
-  ;; Should *not* raise an exception due to the stateful (set!)
+  ;Should *not* raise an exception due to the stateful (set!)
   (with-handlers ([exn:fail:user? (lambda (e) (test-fail e))])
                  (func! 
                    (co-test3 arg1)
                    (yield arg1)
                    (set! arg1 2)
                    (yield arg2))
-                 (test-equal (co-test3 1) 1)
-                 (test-equal (co-test3) 2)
-                 (test-true (not (co-test3)))))
+                 (test-equal? (co-test3 1) 1)
+                 (test-equal? (co-test3) 2)
+                 (test-true? (not (co-test3)))))
 ;;**************************************
 
 
@@ -271,7 +273,7 @@
 ;;TEST
 (when (defined? *run-3-tests*)
   (let ([ch (channel)])
-    (test-true (async-channel? ch))))
+    (test-true? (async-channel? ch))))
 ;;**************************************
 
 
@@ -287,12 +289,12 @@
   (let ([ch (channel)])
     (async-channel-put ch "teststring")
     (let ([ret (<- ch #f)])
-      (test-equal ret "teststring")))
+      (test-equal? ret "teststring")))
 
   (let ([ch (channel)])
     (async-channel-put ch "teststring2")
     (let ([ret (<- ch)])
-      (test-equal ret "teststring2"))))
+      (test-equal? ret "teststring2"))))
 ;;**************************************
 
 
@@ -306,12 +308,12 @@
   (let ([ch (channel)])
     (-> ch "teststring")
     (let ([ret (<- ch #f)])
-      (test-equal ret "teststring")))
+      (test-equal? ret "teststring")))
 
   (let ([ch (channel)])
     (-> ch "teststring2")
     (let ([ret (<- ch)])
-      (test-equal ret "teststring2"))))
+      (test-equal? ret "teststring2"))))
 ;;**************************************
 
 
@@ -356,68 +358,68 @@
 
 ;;TEST
 (when (defined? *run-3-tests*)
-  ;; Sanity test
-  (test-equal (get-dp-data) #f)
+  ;Sanity test
+  (test-equal? (get-dp-data) #f)
 
-  ;; Make a datapool
+  ;Make a datapool
   (let ([num-threads 2]
         [*datapool-environment-data* (make-dp-data num-threads)])
 
-    ;; Verify threads exist
+    ;Verify threads exist
     (for ([i num-threads])
-         (test-true 
+         (test-true? 
            (thread? 
              (car (vector-ref (vector-ref (get-dp-data) 0) i)))))
 
-    ;; Verify the threads are alive
+    ;Verify the threads are alive
     (for ([i num-threads])
-         (test-true (not (thread-dead? 
-                           (car 
-                             (vector-ref 
-                               (vector-ref (get-dp-data) 0) 
-                               i))))))
+         (test-true? (not (thread-dead? 
+                            (car 
+                              (vector-ref 
+                                (vector-ref (get-dp-data) 0) 
+                                i))))))
 
-    ;; Verify task queues exist
+    ;Verify task queues exist
     (for ([i num-threads])
-         (test-true 
+         (test-true? 
            (queue? 
              (cdr (vector-ref (vector-ref (get-dp-data) 0) i)))))
 
-    ;; Verify task queue semaphores exist
+    ;Verify task queue semaphores exist
     (for ([i num-threads])
-         (test-true 
+         (test-true? 
            (semaphore? 
              (cddr (vector-ref (vector-ref (get-dp-data) 0) i)))))
 
-    ;; Verify hash table of message handlers exists
-    (test-true 
+    ;Verify hash table of message handlers exists
+    (test-true? 
       (hash? 
         (car (vector-ref (get-dp-data) 1))))
 
-    ;; Verify hash table semaphore exists
-    (test-true 
+    ;Verify hash table semaphore exists
+    (test-true? 
       (semaphore? 
         (cdr (vector-ref (get-dp-data) 1))))
 
-    ;; Verify parent->dp channel exists
-    (test-true 
+    ;Verify parent->dp channel exists
+    (test-true? 
       (async-channel? 
         (car (vector-ref (get-dp-data) 2))))
 
-    ;; Verify dp->parent channel exists
-    (test-true 
+    ;Verify dp->parent channel exists
+    (test-true? 
       (async-channel? 
         (cdr (vector-ref (get-dp-data) 2))))
 
-    ;; Verify we can kill the datapool environment
+    ;Verify we can kill the datapool environment
     (close-dp *datapool-environment-data*)
 
     (for ([i num-threads])
-         (test-true (thread-dead? 
-                      (car 
-                        (vector-ref 
-                          (vector-ref (get-dp-data) 0) 
-                          i))))))
+         (test-true? (thread-dead? 
+                       (car 
+                         (vector-ref 
+                           (vector-ref (get-dp-data) 0) 
+                           i)))))))
 ;;**************************************
 
 
@@ -431,8 +433,8 @@
   (let ([num-threads 2]
         [*datapool-environment-data* (make-dp-data num-threads)])
 
-    ;; Check that parent->dp channel exists
-    (test-true (async-channel? (get-dp-channel *datapool-environment-data*)))
+    ;Check that parent->dp channel exists
+    (test-true? (async-channel? (get-dp-channel *datapool-environment-data*)))
     (close-dp *datapool-environment-data*)))
 ;;**************************************
 
